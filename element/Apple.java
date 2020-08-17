b7bfaaada7771b5058f18e73fea1f8c445447dcd
Krzysztof Gruszczyński <38075691+Morph21@users.noreply.github.com>
2020-08-17 15:12:38 +0200

    Added autosave function

---
+++ element/Apple.java
@@ -0,0 +1,36 @@
+package pl.morph.ai.snake.element;
+
+import java.io.Serializable;
+
+public class Apple implements Serializable {
+    private int apple_x;
+    private int apple_y;
+
+    public Apple (int apple_x, int apple_y) {
+        this.apple_x = apple_x;
+        this.apple_y = apple_y;
+    }
+
+    public int getApple_x() {
+        return apple_x;
+    }
+
+    public Apple setApple_x(int apple_x) {
+        this.apple_x = apple_x;
+        return this;
+    }
+
+    public int getApple_y() {
+        return apple_y;
+    }
+
+    public Apple setApple_y(int apple_y) {
+        this.apple_y = apple_y;
+        return this;
+    }
+
+    public Apple clone() {
+        Apple apple = new Apple(apple_x, apple_y);
+        return apple;
+    }
+}
---
+++ page/SnakeGame.java
@@ -0,0 +1,68 @@
+package pl.morph.ai.snake.page;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SnakeGame extends JFrame {
+
+    public SnakeGame() {
+
+        initUI();
+    }
+
+    //Set to false if you want AI to play the game
+    private final boolean humanPlaying = false;
+    private final int AISnakesCount = 2000;
+
+    private void initUI() {
+        if (humanPlaying) {
+            initHumanUI();
+        } else {
+            initAIUI();
+        }
+
+        setResizable(false);
+        pack();
+
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void initHumanUI() {
+        GridLayout gridLayout = new GridLayout(1, 2);
+        JPanel ctrl = new JPanel(gridLayout);
+        Scores scores = new Scores();
+        ctrl.add(scores);
+        ctrl.add(new Board(scores, humanPlaying, null));
+        Container cnt = getContentPane();
+
+        cnt.add(ctrl, BorderLayout.CENTER);
+    }
+
+    private void initAIUI() {
+        GridLayout gridLayout = new GridLayout(1, 2);
+        JPanel mainLayout = new JPanel(gridLayout);
+        Scores scores = new Scores();
+        mainLayout.add(scores);
+
+        FlowLayout boardLayout = new FlowLayout();
+
+        JPanel board = new JPanel(boardLayout);
+        board.add(new Board(scores, humanPlaying, AISnakesCount));
+
+        mainLayout.add(board);
+        Container cnt = getContentPane();
+
+        cnt.add(mainLayout, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new SnakeGame();
+            ex.setVisible(true);
+        });
+    }
+}
